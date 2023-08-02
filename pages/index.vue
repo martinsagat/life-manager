d6fc289cfb91b255b705a7b884f582f1aa015f34
Martin Sagat <martin.sagat@outlook.com.au>
2023-08-03 00:50:51 +1000

    add vuetify to the project

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <h1>Life Manager</h1>
+    <p>Demo application</p>
+  </div>
+</template>
+
+<style lang="scss">
+@use './settings';
+</style>
