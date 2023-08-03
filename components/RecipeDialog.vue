44970be2930e1d86a7919a359b66ced822f2aeb7
Martin Sagat <martin.sagat@outlook.com.au>
2023-08-03 11:30:09 +1000

    handle validation for adding a recipe

--- components/RecipeDialog.vue
+++ components/RecipeDialog.vue
@@ -1,31 +1,44 @@
 <script setup lang="ts">
 import { useRecipesStore } from '@/store/recipes'
 import { useIngredientsStore } from '@/store/ingredients';
+import { useField, useForm } from 'vee-validate'
+import { Ingredient, Recipe } from 'types';
+
 const recipesStore = useRecipesStore()
 const ingredientsStore = useIngredientsStore()
 
+const formRef = ref<any>(null);
 const dialog = ref(false)
-const recipe = ref({
-  id: '',
-  name: '',
-  ingredients: [],
-})
 
-const ingredients = ref(ingredientsStore.get())
+const availableIngredients = ref(ingredientsStore.get())
 
+const { handleSubmit, resetForm } = useForm({
+  validationSchema: {
+    name(value: string) {
+      if (value?.length >= 2) return true
 
-const onSave = () => {
-  const newRecipe = { ...recipe.value };
-  newRecipe.id = Math.random().toString(36).substring(2, 9)
-  recipesStore.create(newRecipe)
-    dialog.value = false
-    recipe.value = {
-        id: '',
-        name: '',
-        ingredients: [],
+      return 'Name needs to be at least 2 characters.'
+    },
+    ingredients(value: any) {
+      if (value && value.length > 0) return true
+      return 'Select at least one ingredient.'
     }
-}
+    }
+  })
 
+const name = useField('name')
+const ingredients: any = useField('ingredients')
+
+const submit = handleSubmit(values => {
+  const recipe: Recipe = {
+    id: Math.random().toString(36).substring(2, 9),
+    name: values.name,
+    ingredients: values.ingredients
+  }
+  recipesStore.create(recipe)
+  resetForm()
+  dialog.value = false
+})
 </script>
 
 <template>
@@ -43,6 +56,7 @@ const onSave = () => {
           </v-btn>
         </template>
         <v-card>
+          <v-form ref="formRef" @submit.prevent="submit" >
           <v-card-title>
             <span class="text-h5">New Recipe</span>
           </v-card-title>
@@ -53,7 +67,8 @@ const onSave = () => {
                   cols="12"
                 >
                   <v-text-field
-                    v-model="recipe.name"
+                    v-model="name.value.value"
+                    :error-messages="name.errorMessage.value"
                     label="Name"
                     required
                   ></v-text-field>
@@ -65,8 +80,9 @@ const onSave = () => {
                 >
                   <v-autocomplete
                     chips
-                    v-model="recipe.ingredients"
-                    :items="ingredients"
+                    v-model="ingredients.value.value"
+                    :error-messages="ingredients.errorMessage.value"
+                    :items="availableIngredients"
                     item-text="name"
                     item-value="id"
                     item-title="name"
@@ -89,11 +105,12 @@ const onSave = () => {
               Close
             </v-btn>
             <v-btn
-              @click="onSave"
+              type="submit"
             >
               Save
             </v-btn>
           </v-card-actions>
+        </v-form>
         </v-card>
       </v-dialog>
   </template>
